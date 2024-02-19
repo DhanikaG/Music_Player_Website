cdd3c97f8518f0128a87bea2c807b4725d1f4b57
DhanikaG <86546843+DhanikaG@users.noreply.github.com>
2024-02-19 16:46:18 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,50 @@
+let allMusic = [
+  {
+    name: "Hukum-Thalaivar Alappara",
+    artist: "Anirudh Ravichander",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Ordinary Person",
+    artist: "Anirudh Ravichander & Nikhita Gandhi",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Kurumugil",
+    artist: "Vishal Chandrashekhar & Sai Vignesh",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Husn",
+    artist: "Anuv Jain",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Dandelions",
+    artist: "Ruth B",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Whenever Wherever",
+    artist: "Shakira",
+    img: "music-6",
+    src: "music-6",
+  },
+  {
+    name: "Alone,Pt. ||",
+    artist: "Alan Walker & Ava Max",
+    img: "music-7",
+    src: "music-7",
+  },
+  {
+    name: "Let Me Down Slowly",
+    artist: "Alec Benjamin",
+    img: "music-8",
+    src: "music-8",
+  },
+];
